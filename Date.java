79dea831740cee593a5403b46590930abb299d38
unknown <lrh2003214@gmail.com>
2024-02-11 14:24:53 -0500

    “Date”

---
+++ Date.java
@@ -0,0 +1,27 @@
+public class Date {
+    public static void main(String[] args) {
+        int day=18;
+        String date="Thursday";
+        int year=2019;
+        String month="July";
+        System.out.print("American format:");
+        System.out.print(date);
+        System.out.print(",");
+        System.out.print(month);
+        System.out.print(" ");
+        System.out.print(day);
+        System.out.print(",");
+        System.out.println(year);
+        System.out.print("European format:");
+        System.out.print(date);
+        System.out.print(" ");
+        System.out.print(day);
+        System.out.print(" ");
+        System.out.print(month+" ");
+        System.out.print(year);
+
+
+    }
+
+}
+
